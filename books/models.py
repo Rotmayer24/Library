c9dd538d78b88c103184022e0d563886b32271c2
Rotmayer24 <demkovychvovan@gmail.com>
2026-01-14 21:54:38 +0100

    fix admin registration and model meta

--- books/models.py
+++ books/models.py
@@ -1,3 +1,16 @@
+from enum import unique
 from django.db import models
 
-# Create your models here.
+# create your models here.
+
+class LibraryBook(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    isbn = models.CharField(max_length=100, unique=True)
+    available = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ['author']
+
+    def __str__(self):
+        return self.title
